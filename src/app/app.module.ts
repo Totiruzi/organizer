12829d9a3939c9ba48dcb8c98da42c1905fb940d
Totiruzi <totiruzi@yahoo.com>
2022-03-01 01:12:44 +0300

    Add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module'
 import { AspirationModule } from 'src/app/aspiration/aspiration.module'
 import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AspirationModule],
+  imports: [BrowserModule, AppRoutingModule, AspirationModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
